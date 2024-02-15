64b765d69c7ebadf1abfd3860c3a295093c10ca2
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2024-02-15 14:23:59 +0000

    s

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
+import React, { useState } from 'react';
+
+const Selection = ({ applyColor }) => {
+  const [style, setStyle] = useState({ background: '' });
+
+  const updateSelectionStyle = (nextBackground) => {
+    setStyle(nextBackground);
+  }
+
+  return (
+    <div className="fix-box" style={style} onClick={() => applyColor(updateSelectionStyle)}>
+      Selection Box
+    </div>
+  );
+}
+
+export default Selection;
 
